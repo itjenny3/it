a83c5b029e192761a5886785b3d9ec7743439d48
itjenny3 <itjenny3@gmail.com>
2013-07-12 10:29:48 +0900

    google analytic 적용

--- src/main/java/com/tiny/common/util/Constant.java
+++ src/main/java/com/tiny/common/util/Constant.java
@@ -3,26 +3,8 @@ package com.tiny.common.util;
 public class Constant {
 	// URL
 	public static final String MAIN = "";
-	
-	public static final String LIST = "list";
-	public static final String DOCUMENT = "document";
-	public static final String COMMENT = "comment";
-	public static final String POST = "post";
-	public static final String LIKE = "like";
-	public static final String CANCELLIKE = "cancelLike";
-	public static final String DISLIKE = "dislike";
-	public static final String CANCELDISLIKE = "cancelDislike";
-	public static final String MEMBER = "member";
-	
-	public static final String FRIEND = "friend";
-	
-	public static final String MARKET = "market";
-	public static final String BUY = "buy";
-	
 	public static final String ARTICLE = "article";
 	
+	// CSS
 	public static final String[] CSSLIST = {"color99", "color100", "color110", "color120", "color130"};
-	
-	// ETC
-	public static final Integer ONEPAGELIMIT = 3;
 }
