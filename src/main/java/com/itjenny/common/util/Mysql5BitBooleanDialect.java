ae39791010ea75f7621c25df37250d6df560a9ff
itjenny3 <itjenny3@gmail.com>
2013-07-18 00:55:14 +0900

    checkstyle 수정

--- src/main/java/com/itjenny/common/util/Mysql5BitBooleanDialect.java
+++ src/main/java/com/itjenny/common/util/Mysql5BitBooleanDialect.java
@@ -2,9 +2,9 @@ package com.itjenny.common.util;
 
 import org.hibernate.dialect.MySQL5InnoDBDialect;
 
-public class Mysql5BitBooleanDialect extends MySQL5InnoDBDialect {     
-    public Mysql5BitBooleanDialect() {
-        super();
-        registerColumnType( java.sql.Types.BOOLEAN, "BIT" );
-    }       
+public class Mysql5BitBooleanDialect extends MySQL5InnoDBDialect {
+	public Mysql5BitBooleanDialect() {
+		super();
+		registerColumnType(java.sql.Types.BOOLEAN, "BIT");
+	}
 }
